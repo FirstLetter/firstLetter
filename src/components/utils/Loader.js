fb2166afa1aeb6f613b32ea452496d1eeb66f9c5
Kartik Verma <kvs1297@gmail.com>
2019-10-09 05:25:44 +0530

    added build

--- src/components/utils/Loader.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import 'assets/css/theme.css'
-import 'assets/fontawesome-free-5.11.2-web/css/all.min.css'
-
-export const LoadingComponent = () => {
-    return <i className="app-text-bg-accent font-weight-bold fas fa-spinner fa-spin"></i>
-}
--- src/utils.js
+++
@@ -1,7 +0,0 @@
-export const RequestMethods = {
-    POST: 'post',
-    GET: "get",
-    PUT: "put"
-}
-
-export const CONST_ERROR_NOT_FOUND = "Not Found"
